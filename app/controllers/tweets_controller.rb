448b2b504f9643935345c0784d25084e4fb834b9
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2021-11-03 20:56:22 -0400

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,33 @@
 class TweetsController < ApplicationController
 
+    get '/tweets' do
+        # binding.pry
+        if !session[:user_id]
+            redirect '/login'
+        else
+            @tweets = Tweet.all
+            erb :'tweets/tweets'
+        end
+    end
 
+    get '/tweets/new' do
+        if !session[:user_id]
+            redirect '/login'
+        else
+            erb :'tweets/new'
+        end
+    end
+
+    post '/tweets' do
+        # binding.pry
+        Tweet.create(content:params[:content], user_id:session[:user_id])
+        redirect '/tweets'
+    end
+
+    get '/tweets/:id' do
+        # binding.pry
+        @tweet = Tweet.find(params[:id])
+        @user = User.find(@tweet.user_id)
+        erb :'tweets/show_tweet'
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,54 @@
 class UsersController < ApplicationController
 
+    get '/signup' do
+        # binding.pry
+        if session[:user_id]
+            redirect '/tweets'
+        else
+            erb :"users/create_user"
+        end
+    end
+
+    post '/signup' do
+        # binding.pry
+        if params[:username].empty? || params[:email].empty? || params[:password].empty?
+            redirect '/signup'
+        else
+            @user = User.create(params)
+            # binding.pry
+            session[:user_id] = @user.id
+            redirect '/tweets'
+        end
+    end
+
+    get '/login' do
+        binding.pry
+        if session[:user_id]
+            redirect '/tweets'
+        else
+            erb :"users/login"
+        end
+    end
+
+    post '/login' do
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+        
+            redirect '/tweets'
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
+
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        erb :'users/show'
+    end
 
 end
